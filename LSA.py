ae55c43647b27ac6bd868789a8cd6af26ed7f081
ArthurBorgesNLP <43813171+ArthurBorgesNLP@users.noreply.github.com>
2018-10-21 18:59:28 -0300

    1.1

--- LSA.py
+++ LSA.py
@@ -3,6 +3,7 @@ import nltk
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics.pairwise import euclidean_distances
 
 #definição do conjunto de stopwords
 StopWords = set(stopwords.words('english'))
@@ -17,8 +18,9 @@ TfIdf_matrix = vectorizer.fit_transform(raw_overview_corpus)
 #print(TfIdf_matrix.shape) ==> (45466, 2374466) para corpus_movies.csv
 
 #decomposição da matriz tfidf nas matrizes USV pelo SVD
-SVD = TruncatedSVD(n_components=25, n_iter=100)
-SVD.fit(TfIdf_matrix)
+SVD = TruncatedSVD(n_components=25)
+matriz_documento_conceito = SVD.fit_transform(TfIdf_matrix)
+
 
 #mapeamento dos conceitos(lista_conceitos[]) em conjunto de termos(lista_termos_ordenado)
 lista_termos = vectorizer.get_feature_names()
@@ -27,6 +29,26 @@ for i,conceitos in enumerate(SVD.components_):
     termos_q_definem_conceito = zip(lista_termos,conceitos)
     lista_termos_ordenado = sorted(termos_q_definem_conceito, key=lambda x: x[1], reverse=True) [0:10]
     lista_conceitos.append(lista_termos_ordenado)
+
+
+
+
+def Filmes_similares(entrada_usuario,matriz_documento_conceito):
+    distancia = euclidean_distances(entrada_usuario.reshape(1,-1),matriz_documento_conceito)
+    pares = enumerate(distancia[0])
+    similares_ordenados = sorted(pares, key = lambda item: item[1])[0:5]
+    return similares_ordenados
+
+
+
+
+
+
+
+
+
+
+
 
 #exportando modelo para um arquivo CSV
 
@@ -38,3 +60,20 @@ Saida_fitted_to_csv = {'concept': [i for i in range(0,len(lista_conceitos))],
 data_to_file = pandas.DataFrame(Saida_fitted_to_csv, columns = ['concept','listas_de_termos','listas_de_valores_de_termos'])
 
 data_to_file.to_csv (r'concepts.csv', index = None, header=True)
+
+
+#printando o overview das 5 recomendações
+entrada_usuario = "A film about love sex drugs"
+entrada_transformada = SVD.transform(vectorizer.transform([entrada_usuario]))[0]
+recomendacoes = Filmes_similares(entrada_transformada,matriz_documento_conceito)
+id_documento,similaridade = recomendacoes[0]
+id_documento1,similaridade = recomendacoes[1]
+id_documento2,similaridade = recomendacoes[2]
+id_documento3,similaridade = recomendacoes[3]
+id_documento4,similaridade = recomendacoes[4]
+
+print(raw_overview_corpus[id_documento])
+print(raw_overview_corpus[id_documento1])
+print(raw_overview_corpus[id_documento2])
+print(raw_overview_corpus[id_documento3])
+print(raw_overview_corpus[id_documento4])
